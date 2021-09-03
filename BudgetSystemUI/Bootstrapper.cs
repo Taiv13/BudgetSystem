6207b81377c2506decc465f1fd303e64e994ed5e
Zach Taivalmaa <ztman_123@yahoo.com>
2021-09-03 15:52:06 -0500

    Add project files.

---
+++ BudgetSystemUI/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using BudgetSystemUI.ViewModels;
+
+namespace BudgetSystemUI
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer container;
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            container = new SimpleContainer();
+
+            container.Singleton<IWindowManager, WindowManager>();
+            container.Singleton<IEventAggregator, EventAggregator>();
+
+            container.PerRequest<ShellViewModel>();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
